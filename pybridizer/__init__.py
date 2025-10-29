219cf4370930a1c77dda33254bc33795ae9df85f
ct_ucl <c.trivedi@ucl.ac.uk>
2025-10-29 16:51:21 +0000

    added imports to init

---
+++ pybridizer/__init__.py
@@ -0,0 +1,12 @@
+from .pybridizer import (
+    read_fasta,
+    create_oligos,
+    blast_oligos,
+    fetch_geneIDs,
+    adjacency_filter,
+    filter_and_rank,
+    add_hairpin,
+    plot_sequence_alignment
+)
+
+__version__ = "0.1.0"
