87e876226cc480abdc7a28435317dc4f8eab9ac8
ct_ucl <c.trivedi@ucl.ac.uk>
2025-10-31 14:37:15 +0000

    modified setup for pip

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pybridizer",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "biopython>=1.79",
